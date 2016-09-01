9cdcb60b900abb819bef59a314d6f30173a25d92
Alexandre DUPONCHEL <alexandre.duponchel@gmail.com>
2016-09-01 21:37:17 +0200

    Provider factorisation

--- src/main/java/fr/redrelay/dwrc/recipeguiprovider/RecipeGuiProviderWorkbench.java
+++ src/main/java/fr/redrelay/dwrc/recipeguiprovider/RecipeGuiProviderWorkbench.java
@@ -1,37 +1,22 @@
 package fr.redrelay.dwrc.recipeguiprovider;
 
-import java.util.List;
-
-import fr.redrelay.dwrc.registry.recipegui.IRecipeGuiProvider;
+import fr.redrelay.dwrc.registry.recipegui.RecipeGuiProvider;
 import fr.redrelay.dwrc.registry.recipegui.builder.RecipeGuiBuilder;
-import fr.redrelay.dwrc.registry.recipegui.gui.IRecipeGui;
-import fr.redrelay.dwrc.registry.recipegui.gui.RecipeGui;
-import net.minecraft.client.gui.GuiButton;
 import net.minecraft.client.gui.inventory.GuiContainer;
 import net.minecraft.client.gui.inventory.GuiCrafting;
-import net.minecraftforge.common.config.Configuration;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
 
 @SideOnly(Side.CLIENT)
-public class RecipeGuiProviderWorkbench implements IRecipeGuiProvider {
+public class RecipeGuiProviderWorkbench extends RecipeGuiProvider {
 
-	private final RecipeGuiBuilder builder = new RecipeGuiBuilder("workbench", 96, 59, 148, 59, 124, 64);
+	public RecipeGuiProviderWorkbench() {
+		super(new RecipeGuiBuilder("workbench", 96, 59, 148, 59, 124, 64));
+	}
 	
 	@Override
 	public boolean accept(GuiContainer gui) {
 		return gui instanceof GuiCrafting;
-	}
-
-	@Override
-	public IRecipeGui getRecipeGui(GuiContainer gui, List<GuiButton> listButton) {
-		return new RecipeGui(gui, listButton, builder);
-	}
-
-	@Override
-	public void onConfigChanged(Configuration config) {
-		builder.onConfigChanged(config);
-		
 	}
 
 }
---
+++ src/main/java/fr/redrelay/dwrc/registry/recipegui/RecipeGuiProvider.java
@@ -0,0 +1,30 @@
+package fr.redrelay.dwrc.registry.recipegui;
+
+import java.util.List;
+
+import fr.redrelay.dwrc.registry.recipegui.builder.RecipeGuiBuilder;
+import fr.redrelay.dwrc.registry.recipegui.gui.IRecipeGui;
+import fr.redrelay.dwrc.registry.recipegui.gui.RecipeGui;
+import net.minecraft.client.gui.GuiButton;
+import net.minecraft.client.gui.inventory.GuiContainer;
+import net.minecraftforge.common.config.Configuration;
+
+public abstract class RecipeGuiProvider implements IRecipeGuiProvider {
+
+	protected final RecipeGuiBuilder builder;
+	
+	public RecipeGuiProvider(RecipeGuiBuilder builder) {
+		this.builder = builder;
+	}
+
+	@Override
+	public IRecipeGui getRecipeGui(GuiContainer gui, List<GuiButton> listButton) {
+		return new RecipeGui(gui, listButton, builder);
+	}
+
+	@Override
+	public void onConfigChanged(Configuration config) {
+		this.builder.onConfigChanged(config);
+	}
+
+}
